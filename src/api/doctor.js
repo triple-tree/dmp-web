6d02617e1a131794adc7618e9f3f52ea2fd57e06
liudonghua123 <liudonghua123@gmail.com>
2019-06-04 14:53:25 +0800

    add doctor api

---
+++ src/api/doctor.js
@@ -0,0 +1,43 @@
+import api from './index'
+import { axios } from '@/utils/request'
+import { searchParasToString } from '@/utils/helper/url'
+
+export function doctorAll(data, search) {
+  return axios({
+    url: `${api.doctorAll}${search ? '?' + searchParasToString(search) : ''}`,
+    method: 'get',
+    data,
+  })
+}
+
+export function doctorDetail(data, search) {
+  return axios({
+    url: `${api.doctorDetail}${search ? '?' + searchParasToString(search) : ''}`,
+    method: 'get',
+    data,
+  })
+}
+
+export function doctorAudit(data, search) {
+  return axios({
+    url: `${api.doctorAudit}${search ? '?' + searchParasToString(search) : ''}`,
+    method: 'post',
+    data,
+  })
+}
+
+export function doctorAdd(data, search) {
+  return axios({
+    url: `${api.doctorAdd}${search ? '?' + searchParasToString(search) : ''}`,
+    method: 'post',
+    data,
+  })
+}
+
+export function doctorSelectDoctorByName(data, search) {
+  return axios({
+    url: `${api.doctorSelectDoctorByName}${search ? '?' + searchParasToString(search) : ''}`,
+    method: 'post',
+    data,
+  })
+}
