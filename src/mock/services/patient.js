eb20b6244c28313260ce9ddd1433103a25bf5bb2
liudonghua123 <liudonghua123@gmail.com>
2019-06-04 11:26:57 +0800

    update patient mock

--- src/mock/services/patient.js
+++ src/mock/services/patient.js
@@ -2,7 +2,7 @@ import Mock, { Random } from 'mockjs2'
 import { builder, getQueryParameters, getBody } from '../util'
 
 const total = Random.integer(100, 200)
-let patients = Mock.mock({
+const patients = Mock.mock({
   [`patients|${total}`]: [
     {
       id: () => Random.id(),
@@ -30,31 +30,6 @@ let patients = Mock.mock({
 
 // 3.1.1.	全部患者
 const patientAll = options => {
-  patients = Mock.mock({
-    [`patients|${total}`]: [
-      {
-        id: () => Random.id(),
-        identityNumber: () => Random.id(),
-        name: () => Random.cname(),
-        gender: () => Random.integer(0, 1),
-        dateOfBirth: () => Random.date('yyyy-MM-dd'),
-        phoneNumber: () => Mock.mock({ regexp: /152\d{9}/ }).regexp,
-        doctorId: () => Random.id(),
-        hospitalId: () => Random.id(),
-        hasDiabetes: () => Random.integer(0, 1),
-        hasHypertension: () => Random.integer(0, 1),
-        hasStroke: () => Random.integer(0, 1),
-        hasAscvd: () => Random.integer(0, 1),
-        hasCopd: () => Random.integer(0, 1),
-        hasDyslipidemia: () => Random.integer(0, 1),
-        province: () => Random.province(),
-        city: () => Random.city(),
-        county: () => Random.county(),
-        detailAddress: '天桥大街1号',
-        createDate: () => Random.date('yyyy-MM-dd'),
-      },
-    ],
-  }).patients
   const queryParameters = getQueryParameters(options) || {}
   if (queryParameters && !queryParameters.pageNo) {
     queryParameters.pageNo = 1
