852af1515d931db9a19f448a8da0e25e8c6f2721
liudonghua123 <liudonghua123@gmail.com>
2019-06-05 09:57:37 +0800

    clean up page

--- src/components/tools/UserMenu.vue
+++ src/components/tools/UserMenu.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="user-wrapper">
     <div class="content-box">
+      <!--
       <a href="https://pro.loacg.com/docs/getting-started" target="_blank">
         <span class="action">
           <a-icon type="question-circle-o"></a-icon>
         </span>
       </a>
       <notice-icon class="action"/>
+      -->
       <a-dropdown>
         <span class="action ant-dropdown-link user-dropdown-menu">
           <a-avatar class="avatar" size="small" :src="avatar()"/>
           <span>{{ nickname() }}</span>
         </span>
         <a-menu slot="overlay" class="user-dropdown-menu-wrapper">
+          <!-- 
           <a-menu-item key="0">
             <router-link :to="{ name: 'center' }">
               <a-icon type="user"/>
@@ -29,7 +32,8 @@
             <a-icon type="setting"/>
             <span>测试</span>
           </a-menu-item>
-          <a-menu-divider/>
+          <a-menu-divider/> 
+          -->
           <a-menu-item key="3">
             <a href="javascript:;" @click="handleLogout">
               <a-icon type="logout"/>
@@ -61,17 +65,19 @@ export default {
         title: '提示',
         content: '真的要注销登录吗 ?',
         onOk() {
-          return that.Logout({}).then(() => {
-            window.location.reload()
-          }).catch(err => {
-            that.$message.error({
-              title: '错误',
-              description: err.message,
+          return that
+            .Logout({})
+            .then(() => {
+              window.location.reload()
             })
-          })
+            .catch(err => {
+              that.$message.error({
+                title: '错误',
+                description: err.message,
+              })
+            })
         },
-        onCancel() {
-        },
+        onCancel() {},
       })
     },
   },
